f62b3882a7ab7f8ab49efe05a58e5da6f05f6227
weiglszonja <wszonja@gmail.com>
2024-01-04 15:50:58 +0100

    small refactor

--- src/turner_lab_to_nwb/asap_tdt/asap_tdtnwbconverter.py
+++ src/turner_lab_to_nwb/asap_tdt/asap_tdtnwbconverter.py
@@ -8,7 +8,7 @@ from turner_lab_to_nwb.asap_tdt.interfaces import ASAPTdtRecordingInterface
 
 
 class AsapTdtNWBConverter(NWBConverter):
-    """Primary conversion class for my extracellular electrophysiology dataset."""
+    """Primary conversion class for Turner's extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
         Recording=ASAPTdtRecordingInterface,
