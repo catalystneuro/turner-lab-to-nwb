9eab5c12db8bab0e8220a2d85b5d35c4d8ac3dd2
weiglszonja <wszonja@gmail.com>
2024-01-04 15:44:58 +0100

    add stim_site parameter to determine whether we have plexon data from GPi

--- src/turner_lab_to_nwb/asap_tdt/asap_tdt_convert_session.py
+++ src/turner_lab_to_nwb/asap_tdt/asap_tdt_convert_session.py
@@ -13,9 +13,13 @@ def session_to_nwb(
     data_list_file_path: FilePathType,
     vl_plexon_file_path: FilePathType,
     gpi_plexon_file_path: FilePathType,
+    session_id: str,
+    stim_site: str,
     stub_test: bool = False,
 ):
     """
+    Convert a session of data to NWB.
+
     Parameters
     ----------
     nwbfile_path : FilePathType
@@ -28,6 +32,10 @@ def session_to_nwb(
         The path to Plexon file (.plx) containing the spike sorted data from VL.
     gpi_plexon_file_path : FilePathType
         The path to Plexon file (.plx) containing the spike sorted data from GPi.
+    session_id : str
+        The unique identifier for the session.
+    stim_site : str
+        The name of the stimulation site (either None or "GPi").
     stub_test : bool, optional
         Whether to run the conversion in stub test mode, by default False.
     """
@@ -38,32 +46,38 @@ def session_to_nwb(
 
     # Add Recording
     source_data.update(
-        dict(Recording=dict(file_path=str(ecephys_file_path), electrode_metadata_file_path=str(data_list_file_path)))
+        dict(Recording=dict(file_path=str(ecephys_file_path), data_list_file_path=str(data_list_file_path)))
     )
     conversion_options.update(dict(Recording=dict(stub_test=stub_test)))
 
     # Add Sorting
-    source_data.update(
-        dict(
-            SortingVL=dict(file_path=str(vl_plexon_file_path)),
-            SortingGPi=dict(file_path=str(gpi_plexon_file_path)),
-        )
+    conversion_options_sorting = dict(
+        stub_test=stub_test,
+        write_as="processing",
+        units_description="The units were sorted using the Plexon Offline Sorter v3.",
     )
-    conversion_options.update(
-        dict(
-            SortingVL=dict(stub_test=stub_test),
-            SortingGPi=dict(stub_test=stub_test),
+    if stim_site is None:
+        source_data.update(
+            dict(
+                SortingVL=dict(file_path=str(vl_plexon_file_path)),
+                SortingGPi=dict(file_path=str(gpi_plexon_file_path)),
+            )
         )
-    )
+        conversion_options.update(dict(SortingVL=conversion_options_sorting, SortingGPi=conversion_options_sorting))
+    # If stimulation site is GPi, we only have spike sorting date from VL region, the plexon file for GPi is empty
+    elif stim_site == "GPi":
+        source_data.update(dict(SortingVL=dict(file_path=str(vl_plexon_file_path))))
+        conversion_options.update(dict(SortingVL=conversion_options_sorting))
 
     converter = AsapTdtNWBConverter(source_data=source_data)
 
     metadata = converter.get_metadata()
     # For data provenance we can add the time zone information to the conversion if missing
+
     session_start_time = metadata["NWBFile"]["session_start_time"]
     tzinfo = tz.gettz("US/Pacific")
     metadata["NWBFile"].update(
-        session_id=ecephys_file_path.stem.replace("_", "-"),
+        session_id=str(session_id).replace("_", "-"),
         session_start_time=session_start_time.replace(tzinfo=tzinfo),
     )
 
@@ -80,16 +94,33 @@ def session_to_nwb(
 
 if __name__ == "__main__":
     # Parameters for conversion
-    folder_path = Path("/Volumes/t7-ssd/Turner/Previous_PD_Project_sample/I_160615")
+    # The root folder containing the sessions
+    folder_path = Path("/Volumes/t7-ssd/Turner/Previous_PD_Project_sample")
 
-    tank_file_path = folder_path / "Gaia_I_160615_2.Tbk"
+    # The date string that identifies the session
+    date_string = "160624"
+    # The identifier of the session to be converted
+    session_id = f"I_{date_string}_2"
+
+    # The path to a single TDT Tank file (.Tbk)
+    tank_file_path = folder_path / f"I_{date_string}" / f"Gaia_{session_id}.Tbk"
+
+    # The path to the electrode metadata file (.xlsx)
     data_list_file_path = Path("/Volumes/t7-ssd/Turner/Previous_PD_Project_sample/Isis_DataList_temp.xlsx")
 
-    vl_plexon_file_path = folder_path / "I_160615_2_Chans_1_1.plx"
-    gpi_plexon_file_path = folder_path / "I_160615_2_Chans_17_32.plx"
+    # The plexon file with the spike sorted data from VL
+    vl_plexon_file_path = folder_path / f"I_{date_string}" / f"{session_id}_Chans_1_16.plx"
+    # The plexon file with the spike sorted data from GPi
+    gpi_plexon_file_path = folder_path / f"I_{date_string}" / f"{session_id}_Chans_24_24.plx"
 
-    nwbfile_path = Path("/Volumes/t7-ssd/nwbfiles/stub_Gaia_I_160615_2.nwb")
-    stub_test = True
+    # The name of the stimulation site (either None or "GPi")
+    stim_site = "GPi"
+
+    # The path to the NWB file to be created
+    nwbfile_path = Path(f"/Volumes/t7-ssd/nwbfiles/stub_Gaia_{session_id}.nwb")
+
+    # For testing purposes, set stub_test to True to convert only a stub of the session
+    stub_test = False
 
     session_to_nwb(
         nwbfile_path=nwbfile_path,
@@ -97,5 +128,7 @@ if __name__ == "__main__":
         data_list_file_path=data_list_file_path,
         vl_plexon_file_path=vl_plexon_file_path,
         gpi_plexon_file_path=gpi_plexon_file_path,
+        session_id=session_id,
+        stim_site=stim_site,
         stub_test=stub_test,
     )
