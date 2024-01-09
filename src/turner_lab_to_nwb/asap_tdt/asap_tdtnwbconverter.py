aae711ea5c18e9b5cf5528d2efd7e2ded963d399
weiglszonja <wszonja@gmail.com>
2024-01-09 10:40:26 +0100

    fix indentation of run_conversion

--- src/turner_lab_to_nwb/asap_tdt/asap_tdtnwbconverter.py
+++ src/turner_lab_to_nwb/asap_tdt/asap_tdtnwbconverter.py
@@ -27,69 +27,67 @@ class AsapTdtNWBConverter(NWBConverter):
 
         return metadata
 
+    def run_conversion(
+        self,
+        nwbfile_path: Optional[str] = None,
+        nwbfile: Optional[NWBFile] = None,
+        metadata: Optional[dict] = None,
+        overwrite: bool = False,
+        conversion_options: Optional[dict] = None,
+    ) -> None:
+        # Rename unit properties to have descriptive names
+        unit_properties_mapping = {
+            "Area": "brain_area",
+            "Quality": "unit_quality",
+            "Quality (post-sorting)": "unit_quality_post_sorting",
+            "GoodPeriod": "good_period",
+        }
 
-def run_conversion(
-    self,
-    nwbfile_path: Optional[str] = None,
-    nwbfile: Optional[NWBFile] = None,
-    metadata: Optional[dict] = None,
-    overwrite: bool = False,
-    conversion_options: Optional[dict] = None,
-) -> None:
+        # Map unit quality values to more descriptive names
+        quality_values_map = dict(
+            A="excellent",
+            B="good",
+            C="not good enough to call single-unit",
+        )
+        electrode_metadata = self.data_interface_objects["Recording"]._electrode_metadata
+        electrode_metadata["Quality"] = electrode_metadata["Quality"].replace(quality_values_map)
+        electrode_metadata["Quality (post-sorting)"] = electrode_metadata["Quality (post-sorting)"].replace(
+            quality_values_map
+        )
 
-    # Rename unit properties to have descriptive names
-    unit_properties_mapping = {
-        "Area": "brain_area",
-        "Quality": "unit_quality",
-        "Quality (post-sorting)": "unit_quality_post_sorting",
-        "GoodPeriod": "good_period",
-    }
+        num_units = 0
+        sorting_interfaces = [
+            interface_name for interface_name in self.data_interface_objects if "Sorting" in interface_name
+        ]
+        for interface_name in sorting_interfaces:
+            target_name = interface_name.replace("Sorting", "")
+            sorting_metadata = electrode_metadata[electrode_metadata["Target"] == target_name]
 
-    # Map unit quality values to more descriptive names
-    quality_values_map = dict(
-        A="excellent",
-        B="good",
-        C="not good enough to call single-unit",
-    )
-    electrode_metadata = self.data_interface_objects["Recording"]._electrode_metadata
-    electrode_metadata["Quality"] = electrode_metadata["Quality"].replace(quality_values_map)
-    electrode_metadata["Quality (post-sorting)"] = electrode_metadata["Quality (post-sorting)"].replace(
-        quality_values_map
-    )
+            sorting_interface = self.data_interface_objects[interface_name]
+            sorting_extractor = sorting_interface.sorting_extractor
 
-    num_units = 0
-    sorting_interfaces = [
-        interface_name for interface_name in self.data_interface_objects if "Sorting" in interface_name
-    ]
-    for interface_name in sorting_interfaces:
-        target_name = interface_name.replace("Sorting", "")
-        sorting_metadata = electrode_metadata[electrode_metadata["Target"] == target_name]
+            if len(sorting_metadata) != sorting_extractor.get_num_units():
+                # TODO: how to deal with the fact that the Vla data has 2 units but only one of them in the metadata
+                area_value = sorting_metadata["Area"].values[0]
+                sorting_extractor.set_property(
+                    key="brain_area",
+                    values=[area_value] * sorting_extractor.get_num_units(),
+                )
+                continue
+            for property_name, renamed_property_name in unit_properties_mapping.items():
+                sorting_extractor.set_property(
+                    key=renamed_property_name,
+                    values=sorting_metadata[property_name].values.tolist(),
+                )
+            extractor_unit_ids = sorting_extractor.get_unit_ids()
+            # unit_ids are not unique across sorting interfaces, so we are offsetting them here
+            sorting_extractor._main_ids = extractor_unit_ids + num_units
+            num_units = len(extractor_unit_ids)
 
-        sorting_interface = self.data_interface_objects[interface_name]
-        sorting_extractor = sorting_interface.sorting_extractor
-
-        if len(sorting_metadata) != sorting_extractor.get_num_units():
-            # TODO: how to deal with the fact that the Vla data has 2 units but only one of them in the metadata
-            area_value = sorting_metadata["Area"].values[0]
-            sorting_extractor.set_property(
-                key="brain_area",
-                values=[area_value] * sorting_extractor.get_num_units(),
-            )
-            continue
-        for property_name, renamed_property_name in unit_properties_mapping.items():
-            sorting_extractor.set_property(
-                key=renamed_property_name,
-                values=sorting_metadata[property_name].values.tolist(),
-            )
-        extractor_unit_ids = sorting_extractor.get_unit_ids()
-        # unit_ids are not unique across sorting interfaces, so we are offsetting them here
-        sorting_extractor._main_ids = extractor_unit_ids + num_units
-        num_units = len(extractor_unit_ids)
-
-    super().run_conversion(
-        nwbfile_path=nwbfile_path,
-        nwbfile=nwbfile,
-        metadata=metadata,
-        overwrite=overwrite,
-        conversion_options=conversion_options,
-    )
+        super().run_conversion(
+            nwbfile_path=nwbfile_path,
+            nwbfile=nwbfile,
+            metadata=metadata,
+            overwrite=overwrite,
+            conversion_options=conversion_options,
+        )
