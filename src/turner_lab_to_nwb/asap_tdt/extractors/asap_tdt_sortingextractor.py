701c40beb898fa83b1bc79b496e40dce91ec490c
weiglszonja <wszonja@gmail.com>
2024-01-18 15:23:46 +0100

    add units filtering by location to ASAPTdtSortingExtractor

--- src/turner_lab_to_nwb/asap_tdt/extractors/asap_tdt_sortingextractor.py
+++ src/turner_lab_to_nwb/asap_tdt/extractors/asap_tdt_sortingextractor.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import Optional, Literal
 import numpy as np
+import pandas as pd
 from pymatreader import read_mat
 from spikeinterface import BaseSorting, BaseSortingSegment
 
@@ -13,24 +14,32 @@ class ASAPTdtSortingExtractor(BaseSorting):
     installation_mesg = ""
     name = "tdtsorting"
 
-    def __init__(self, file_path: FilePathType):
+    def __init__(self, file_path: FilePathType, location: Literal["GPi", "VL"] = None):
         """
         Parameters
         ----------
         file_path : FilePathType
             The file path to the MAT file containing the clustered spike times.
+        location: Literal["GPi", "VL"], optional
+            The location of the probe, when specified allows to filter the units by location. By default None.
         """
 
         mat = read_mat(file_path)
         assert "units" in mat, f"The 'units' structure is missing from '{file_path}'."
-        self._units_data = mat["units"]
-        spike_times = self._units_data["ts"]
-        num_units = len(self._units_data["uname"])
+        units_df = pd.DataFrame(mat["units"])
+
+        # filter out units based on location
+        if location is not None:
+            units_df = units_df[units_df["brain_area"].str.contains(location)]
+            if units_df.empty:
+                raise ValueError(f"No units found in '{file_path}' for location '{location}'.")
+
+        unit_names = units_df["uname"].values.tolist()
+        num_units = len(unit_names)
         unit_ids = np.arange(num_units)
-
         sampling_frequency = float(mat["Tanksummary"]["ChanFS"][-1])
-
         BaseSorting.__init__(self, sampling_frequency=sampling_frequency, unit_ids=unit_ids)
+        spike_times = units_df["ts"].values
         sorting_segment = ASAPTdtSortingSegment(
             sampling_frequency=sampling_frequency,
             spike_times=spike_times,
@@ -44,9 +53,11 @@ class ASAPTdtSortingExtractor(BaseSorting):
             "A -> B": "changed to good from excellent based on post-sorting quality",
             "B -> A": "changed to excellent from good based on post-sorting quality",
         }
-        units_quality = [quality_values_map.get(quality, quality) for quality in self._units_data["sort_qual"]]
-        self.set_property(key="unit_quality_post_sorting", values=units_quality)
+        units_quality = units_df["sort_qual"].values.tolist()
+        units_quality_renamed = [quality_values_map.get(quality, quality) for quality in units_quality]
+        self.set_property(key="unit_quality_post_sorting", values=units_quality_renamed)
 
+        units_df["chan"] = units_df["chan"].astype(int)
         unit_properties_mapping = dict(
             uname="unit_name",
             sort="sort_label",
@@ -56,7 +67,7 @@ class ASAPTdtSortingExtractor(BaseSorting):
         for property_name, renamed_property_name in unit_properties_mapping.items():
             self.set_property(
                 key=renamed_property_name,
-                values=self._units_data[property_name],
+                values=units_df[property_name].values.tolist(),
             )
 
 
