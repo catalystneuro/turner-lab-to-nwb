c643b4d31eabf6baa6b568de45ea8d322b85ad27
weiglszonja <wszonja@gmail.com>
2024-01-22 14:06:02 +0100

    add pharmacology to NWBFile metadata

--- src/turner_lab_to_nwb/asap_tdt/asap_tdt_convert_session.py
+++ src/turner_lab_to_nwb/asap_tdt/asap_tdt_convert_session.py
@@ -120,6 +120,8 @@ def session_to_nwb(
     subject_metadata_path = Path(__file__).parent / "asap_tdt_subjects_metadata.yaml"
     subject_metadata = load_dict_from_file(subject_metadata_path)
     assert subject_id in subject_metadata["Subject"], f"Subject {subject_id} is not in the metadata file."
+    pharmacology = subject_metadata["Subject"][subject_id].pop("pharmacology")
+    metadata["NWBFile"].update(pharmacology=pharmacology)
     metadata["Subject"] = subject_metadata["Subject"][subject_id]
 
     # Run conversion
