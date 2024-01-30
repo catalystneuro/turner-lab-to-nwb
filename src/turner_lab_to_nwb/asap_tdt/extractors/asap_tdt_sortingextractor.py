9c5b7f416d7348c26354b77e5dd79091b7ecb011
weiglszonja <wszonja@gmail.com>
2024-01-30 16:34:30 +0100

    make sort_qual optional in units table

--- src/turner_lab_to_nwb/asap_tdt/extractors/asap_tdt_sortingextractor.py
+++ src/turner_lab_to_nwb/asap_tdt/extractors/asap_tdt_sortingextractor.py
@@ -63,9 +63,10 @@ class ASAPTdtSortingExtractor(BaseSorting):
             "A -> B": "changed to good from excellent based on post-sorting quality",
             "B -> A": "changed to excellent from good based on post-sorting quality",
         }
-        units_quality = units_df["sort_qual"].values.tolist()
-        units_quality_renamed = [quality_values_map.get(quality, quality) for quality in units_quality]
-        self.set_property(key="unit_quality_post_sorting", values=units_quality_renamed)
+        if "sort_qual" in units_df:
+            units_quality = units_df["sort_qual"].values.tolist()
+            units_quality_renamed = [quality_values_map.get(quality, quality) for quality in units_quality]
+            self.set_property(key="unit_quality_post_sorting", values=units_quality_renamed)
 
         units_df["chan"] = units_df["chan"].astype(int)
 
