8ebb4f523b0b65aeea9ed4cd82c61d042bb398e1
weiglszonja <wszonja@gmail.com>
2024-02-19 17:15:26 +0100

    modify filter to gpi_only in sorting

--- src/turner_lab_to_nwb/asap_tdt/interfaces/asap_tdt_sortinginterface.py
+++ src/turner_lab_to_nwb/asap_tdt/interfaces/asap_tdt_sortinginterface.py
@@ -1,5 +1,3 @@
-from typing import Literal
-
 from neuroconv.datainterfaces.ecephys.basesortingextractorinterface import BaseSortingExtractorInterface
 from neuroconv.utils import FilePathType
 
@@ -9,15 +7,15 @@ from turner_lab_to_nwb.asap_tdt.extractors import ASAPTdtSortingExtractor
 class ASAPTdtSortingInterface(BaseSortingExtractorInterface):
     Extractor = ASAPTdtSortingExtractor
 
-    def __init__(self, file_path: FilePathType, location: Literal["GPi", "VL", None] = None, verbose: bool = True):
+    def __init__(self, file_path: FilePathType, gpi_only: bool = True, verbose: bool = True):
         """
         Parameters
         ----------
         file_path: FilePathType
             Path to the MAT file containing the spiking data.
-        location: Literal["GPi", "VL", None], optional
-            The location of the probe, when specified allows to filter the units by location. By default None.
+        gpi_only: bool, optional
+            Determines whether to load only GPi units, by default True.
         verbose: bool, default: True
             Allows verbosity.
         """
-        super().__init__(file_path=file_path, location=location, verbose=verbose)
+        super().__init__(file_path=file_path, gpi_only=gpi_only, verbose=verbose)
